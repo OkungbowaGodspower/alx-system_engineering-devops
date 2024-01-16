1e21ceb637111487599e5c0453f1a9e324924ceb
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2024-01-16 17:36:46 +0100

    Print titles of first 10 hot posts for a subreddit. Fix for incorrect comment

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-prints the titles of the first 10 hot posts listed for a given subreddit
+It prints the titles of the first 10 hot posts listed for a given subreddit
 """
 
 from requests import get
