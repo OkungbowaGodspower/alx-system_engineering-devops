8c4d6c9a78ce686ff1ee4ceaec13cf922debcad8
Okungbowa Godspower <godspowerokungbowa0@gmail.com>
2024-01-25 15:28:47 +0100

    Increase Nginx server ULIMIT and restart for handling higher traffic

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 # This manuscript increases the amount of traffic an Nginx server can handle
+=======
+# This script increases the amount of traffic an Nginx server can handle
+>>>>>>> 98aa44056218acbe1d446d0c9d4afcb86df1a6a3
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
